f4143077649182ae7308305540586d0014ca10f9
Chase Hilbert <chase@heyspacetime.com>
2019-02-04 22:47:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(word)
   reverse_word = word.split
-  reverse_word.collect do |w|
+  reverse_word.each do |w|
   w.reverse!
 end
-reverse_word.join(" ")
-reverse_word
+new_reverse = reverse_word.join(" ")
+new_reverse
 end
