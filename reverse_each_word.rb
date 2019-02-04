8ee5833bf0ae49457c0cb8c51a5ee0dbc0906006
Chase Hilbert <chase@heyspacetime.com>
2019-02-04 22:35:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(word)
-  word.to_a
-  word.each do |w|
+  reverse_word = word.to_a
+  reverse_word.each do |w|
     w.reverse!
 end
-word.join
-word
+reverse_word.join
+reverse_word
 end
